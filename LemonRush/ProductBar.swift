6c775497bb3999515811509e382dc3813c31e111
cscalcucci <cscal@aphelion-enterprises.com>
2015-11-06 21:20:41 -0500

    First Commit

---
+++ LemonRush/ProductBar.swift
@@ -0,0 +1,21 @@
+//
+//  ProductBar.swift
+//  LemonRush
+//
+//  Created by Christopher Scalcucci on 11/5/15.
+//  Copyright © 2015 Christopher Scalcucci. All rights reserved.
+//
+
+import UIKit
+
+class ProductBar: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
